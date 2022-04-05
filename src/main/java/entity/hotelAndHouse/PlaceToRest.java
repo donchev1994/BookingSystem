075f56dceb8ac7114f0f68b1c7e5e2741cca2f68
donchev <donchev1994@gmail.com>
2022-04-05 12:07:44 +0300

    Add JDBC Connector class and demo

---
+++ src/main/java/entity/hotelAndHouse/PlaceToRest.java
@@ -0,0 +1,22 @@
+package entity.hotelAndHouse;
+
+import entity.city.City;
+import entity.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceToRest {
+    private long id;
+    private String name;
+    private String description;
+    private String tag;
+    private Status status;
+    private long sleepingPlaces;
+    private int stars;
+    private City city;
+}
